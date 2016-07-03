378b8af7cb7f35d2d3cf5fe9fcab286d26820931
chocolat0w0 <shiori.yamazaki.1002@gmail.com>
2016-07-04 05:55:24 +0900

    カードを弱い順に並び替える

--- src/Player.scala
+++ src/Player.scala
@@ -1,4 +1,6 @@
 case class Player(cardsStr: String) {
-  val hands:Array[Card] = cardsStr.split(" ").map(Card(_))
-
+  val hands: Array[Card] = cardsStr.split(" ").map(Card(_))
+  hands.foreach(h => println(h))
+  println("-----ordered-----")
+  hands.sorted.foreach(h => println(h))
 }
