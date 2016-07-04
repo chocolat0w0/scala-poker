7e8a32fdae15314a9ca27ba2fd0bc7037d713fce
chocolat0w0 <shiori.yamazaki.1002@gmail.com>
2016-07-05 05:37:12 +0900

    フラッシュの判定ロジックをストレートに似せる

--- src/PokerHandValidator.scala
+++ src/PokerHandValidator.scala
@@ -8,12 +8,6 @@ trait PokerHandValidator {
   def isValid(cards: Seq[Card]): Option[WinningHand]
 }
 
-sealed case class WinningHand(name: String, strength: Int, card: Card) extends Ordered[WinningHand] {
-  override def compare(that: WinningHand): Int = strength - that.strength
-}
-class Straight(card: Card) extends WinningHand("Straight", 5, card)
-class Flush(card: Card) extends WinningHand("Flush", 6, card)
-
 object Straight extends PokerHandValidator {
   override def isValid(cards: Seq[Card]): Option[WinningHand] = {
     if (cards.zip(cards.tail).forall { case(x, y) => x.rank.strength  + 1 == y.rank.strength }) Option(new Straight(cards.last))
@@ -23,7 +17,14 @@ object Straight extends PokerHandValidator {
 
 object Flush extends PokerHandValidator {
   override def isValid(cards: Seq[Card]): Option[WinningHand] = {
-    if (cards.forall(_.suit.equals(cards.head.suit))) Option(new Flush(cards.last))
+    if (cards.zip(cards.tail).forall {case(x, y) => x.suit.equals(y.suit)}) Option(new Flush(cards.last))
     else None
   }
 }
+
+sealed case class WinningHand(name: String, strength: Int, card: Card) extends Ordered[WinningHand] {
+  override def compare(that: WinningHand): Int = strength - that.strength
+}
+class Straight(card: Card) extends WinningHand("Straight", 5, card)
+class Flush(card: Card) extends WinningHand("Flush", 6, card)
+
