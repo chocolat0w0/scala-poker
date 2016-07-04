ee41b0be57913a5d8503e059157b55298fea5e46
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2016-07-04 22:54:51 +0900

    役に強さを追加、テストパターン追加

--- src/Main.scala
+++ src/Main.scala
@@ -1,6 +1,28 @@
 object Main {
   def main(args: Array[String]) {
-    val player = Player("H-8 S-K D-J S-10 C-Q")
-    player.judge
+//    test("S-4 S-Q H-2 H-J H-3", "High Card", "S-Q")
+//
+//    test("S-4 S-Q H-Q H-J H-3", "One Pair", "S-Q")
+//
+//    test("S-4 S-J H-J H-Q H-4", "Two Pair", "S-J")
+//
+//    test("S-4 S-Q H-4 H-J H-4", "Three Of A Kind", "S-4")
+
+    test("H-2 D-3 S-4 C-5 S-6", "Straight", "S-6")
+
+//    test("H-4 H-Q H-2 H-J H-3", "Flush", "H-Q")
+//
+//    test("H-4 H-Q S-Q D-Q H-4", "Full House", "S-Q")
+//
+//    test("H-4 H-Q S-Q D-Q C-Q", "Four Of A Kind", "S-Q")
+//
+//    test("S-5 S-3 S-6 S-2 S-4", "Straight Flush", "S-6")
+//
+//    test("S-J S-Q S-A S-K S-10", "Royal Straight Flush", "S-A")
+  }
+
+  def test(input: String, winningHand: String, winningCard: String): Unit = {
+    val actual = Player(input).judge
+    assert(winningHand == actual._1 && winningCard == actual._2, input)
   }
 }
