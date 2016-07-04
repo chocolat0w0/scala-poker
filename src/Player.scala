f9b67b38d388234de2d2cac0e14cdfe85595da95
chocolat0w0 <shiori.yamazaki.1002@gmail.com>
2016-07-04 23:36:19 +0900

    最も強い役を判定する

--- src/Player.scala
+++ src/Player.scala
@@ -1,8 +1,14 @@
 case class Player(cardsStr: String) {
   val hands: Seq[Card] = cardsStr.split(" ").map(Card(_)).sorted
 
+  /**
+    * プレイヤーが所持する手札で成立する、最も強い役を返す
+    * @return (役名、役を構成するカードで最も強いもの)
+    */
   def judge: (String, String) = {
-    val winningHand = Straight.isMatch(hands).get
+    val validator = Seq(Straight)
+//    TODO: 強い方から判定して、成立したら以降の判定はしないようにできないかな？
+    val winningHand = validator.flatMap(_.isMatch(hands)).head
     (winningHand.name, winningHand.card.str)
   }
 }
