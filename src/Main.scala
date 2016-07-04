5773c6e3ee72e942c8e1d4a6febbf61d131fd891
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2016-07-05 06:39:05 +0900

    ハイカードを判定

--- src/Main.scala
+++ src/Main.scala
@@ -1,23 +1,23 @@
 object Main {
   def main(args: Array[String]) {
-//    test("S-4 S-Q H-2 H-J H-3", "High Card", "S-Q")
-//
+    test("S-4 S-Q H-2 H-J H-3", "High Card", "S-Q")
+
     test("S-4 S-Q H-Q H-J H-3", "One Pair", "S-Q")
-//
+
     test("S-4 S-J H-J H-Q H-4", "Two Pair", "S-J")
-//
+
     test("S-4 S-Q H-4 H-J H-4", "Three Of A Kind", "S-4")
 
     test("H-2 D-3 S-4 C-5 S-6", "Straight", "S-6")
 
     test("H-4 H-Q H-2 H-J H-3", "Flush", "H-Q")
-//
+
     test("H-4 H-Q S-Q D-Q H-4", "Full House", "S-Q")
-//
+
     test("H-4 H-Q S-Q D-Q C-Q", "Four Of A Kind", "S-Q")
-//
+
     test("S-5 S-3 S-6 S-2 S-4", "Straight Flush", "S-6")
-//
+
     test("S-J S-Q S-A S-K S-10", "Royal Straight Flush", "S-A")
   }
 
